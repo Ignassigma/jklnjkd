ebc30ccd59c7ea4f478ad09ee96b8694b0162123
Ignassigma <Ignas.Griunas2@gmail.com>
2025-11-14 19:13:25 +0000

    first commit

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.createElement('button');
-    button.textContent = 'Click me to disappear!';
-    button.addEventListener('click', function() {
-        this.style.display = 'none';
-    });
-    document.body.appendChild(button);
-});
